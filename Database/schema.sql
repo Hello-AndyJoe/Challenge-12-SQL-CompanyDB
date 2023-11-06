85028a742992f76c2b3c01c15d0b42b369bb6193
Hello-AndyJoe <andrew.joseph.roper@gmail.com>
2023-11-05 17:31:33 -0700

    Added readme, comments and additional adjustments.

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS company_db;
-CREATE DATABASE company_db;
+DROP DATABASE IF EXISTS company_db; -- Deletes the database if it already exist
+CREATE DATABASE company_db; -- Creates a database by the name of company_db
 
 USE company_db;
 
@@ -21,7 +21,7 @@ CREATE TABLE company_role (
 );
 
 CREATE TABLE employee (
-  employee_id INT PRIMARY KEY NOT NULL,
+  employee_id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
@@ -30,35 +30,3 @@ CREATE TABLE employee (
   REFERENCES company_role(role_id)
     ON DELETE SET NULL
 );
-
--- SELECT company_role.role_title, company_role.role_salary, department.dept_name
--- FROM company_role
--- INNER JOIN department ON company_role.dept_id=department.dept_id;
-
--- SELECT employee.first_name, employee.last_name, company_role.role_title
--- FROM employee
--- INNER JOIN company_role ON employee.role_id=company_role.role_id;
-
--- SELECT employee.last_name, manager.last_name AS Manager
--- FROM employee
--- LEFT JOIN employee manager ON employee.manager_id = manager.employee_id;
-
--- SELECT employee.first_name, employee.last_name, company_role.role_title, CONCAT(manager.last_name, ", " , manager.first_name) AS Manager
--- FROM employee
--- LEFT JOIN employee manager ON employee.manager_id = manager.employee_id
--- INNER JOIN company_role ON employee.role_id=company_role.role_id;
-
--- SELECT CONCAT(employee.first_name, " ", employee.last_name) AS Employee, 
--- company_role.role_title, 
--- CONCAT(manager.last_name, ", " , manager.first_name) AS Manager
--- FROM employee
--- LEFT JOIN employee manager ON employee.manager_id = manager.employee_id
--- INNER JOIN company_role ON employee.role_id=company_role.role_id;
-
--- SELECT employee.employee_id,
--- CONCAT(employee.first_name, " ", employee.last_name) AS Employee, 
--- company_role.role_title, 
--- CONCAT(manager.last_name, ", " , manager.first_name) AS Manager
--- FROM employee
--- LEFT JOIN employee manager ON employee.manager_id = manager.employee_id
--- INNER JOIN company_role ON employee.role_id=company_role.role_id;
